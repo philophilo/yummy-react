842296e6dcf5776c748b807df842d64a3a31b514
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-03-20 10:22:02 +0300

    [Delivers #164949162] Add registration  tests

---
+++ src/__tests__/registration/RegistrationForm.test.js
@@ -0,0 +1,39 @@
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import React from 'react';
+ 
+// import Testutils from 'react-addons-test-utils';
+import RegistrationForm from '../../registration/RegistrationForm';
+
+describe('<CategoryForm />', () => {
+    const store = configureMockStore([thunk])({
+      
+    });
+  
+    const props = {
+        onSave: jest.fn(),
+        onChange: jest.fn(),
+        loading: false,
+        errors: {},
+        register: {}
+    };
+    it('should render itself without crashing', () => {
+        const match = { params: {} }
+        mount(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <RegistrationForm {...props} match />
+                </MemoryRouter>
+            </Provider>
+            );
+    });
+
+  });
---
+++ src/__tests__/registration/UserListRow.test.js
@@ -0,0 +1,35 @@
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import React from 'react';
+ 
+// import Testutils from 'react-addons-test-utils';
+import UserListRow from '../../registration/UserListRow';
+
+describe('<UserListRow />', () => {
+    const store = configureMockStore([thunk])({
+      
+    });
+  
+    const props = {
+        user: {}
+    };
+    it('should render itself without crashing', () => {
+        const match = { params: {} }
+        mount(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <UserListRow {...props} match />
+                </MemoryRouter>
+            </Provider>
+            );
+    });
+
+  });
