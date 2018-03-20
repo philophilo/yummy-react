1dd6a6aa2ea53ac683c35a2be03ce62f1430085c
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-03-20 10:36:35 +0300

    Lint category form

--- src/__tests__/recipes/RecipeListRow.test.js
+++ src/__tests__/recipes/RecipeListRow.test.js
@@ -9,39 +9,38 @@ import { MemoryRouter } from 'react-router-dom';
 Enzyme.configure({ adapter: new Adapter() });
 
 import React from 'react';
- 
+
 // import Testutils from 'react-addons-test-utils';
 import RecipeListRow from '../../recipes/RecipeListRow';
 
 describe('<RecipeListRow />', () => {
-    const store = configureMockStore([thunk])({
-      
-    });
-  
-    const props = {
-        onDelete: jest.fn(),
-        recipe: {
-            "category_id": 24,
-            "category_name": "My chicken",
-            "description": "sdfdfsdffsd",
-            "id": 45,
-            "ingredients": [
-                "sdfsdf ",
-                "  sdfsd",
-                " sddfd"
-            ],
-            "recipe_date": "Mon, 19 Mar 2018 09:39:44 GMT",
-            "recipe_name": "ajhbdsvkjnds"
-        }
-    };
-    it('should render itself without crashing', () => {
-        const match = { params: {} }
-        mount(
-            <Provider store={store}>
-                <MemoryRouter>
-                    <RecipeListRow {...props} match />
-                </MemoryRouter>
-            </Provider>);
-    });
+  const store = configureMockStore([thunk])({
 
   });
+
+  const props = {
+    onDelete: jest.fn(),
+    recipe: {
+      'category_id': 24,
+      'category_name': 'My chicken',
+      'description': 'sdfdfsdffsd',
+      'id': 45,
+      ingredients: [
+        'sdfsdf ',
+        '  sdfsd',
+        ' sddfd',
+      ],
+      recipe_date: 'Mon, 19 Mar 2018 09:39:44 GMT',
+      'recipe_name': 'ajhbdsvkjnds',
+    },
+  };
+  it('should render itself without crashing', () => {
+    const match = { params: {} };
+    mount(
+          <Provider store={store}>
+              <MemoryRouter>
+                  <RecipeListRow {...props} match />
+                </MemoryRouter>
+            </Provider>);
+  });
+});
--- src/category/CategoryForm.js
+++ src/category/CategoryForm.js
@@ -4,42 +4,45 @@ import TextInput from '../common/TextInput';
 import SelectInput from '../common/SelectInput';
 import Textarea from '../common/Textarea';
 
-const CategoryForm = ({category, onSave, onChange, loading, errors}) => {
-    return (
-        <div  className='container-fluid body-bg'>
-            <form>
-                <h1>Manage category</h1>
-                <TextInput
-                    name="category_name"
-                    label="CategoryName"
-                    value={category.category_name}
-                    onChange={onChange}
-                    error={errors.title} />
+const CategoryForm = ({ category, onSave, onChange, loading, errors }) => {
+  return (
+    <div className="container-fluid body-bg">
+      <form>
+        <h1>Manage category</h1>
+        <TextInput
+          name="category_name"
+          label="CategoryName"
+          value={category.category_name}
+          onChange={onChange}
+          error={errors.title}
+        />
 
-                <Textarea 
-                    name="category_description"
-                    label="Description"
-                    value={category.category_description}
-                    onChange={onChange}
-                    error={errors.title}/>
-                
-                <input
-                    type="submit"
-                    disabled={loading}
-                    value={loading ? 'Saving...' : 'Save'}
-                    className="btn btn-primary"
-                    onClick={onSave} />
-            </form>
-        </div>
-    );
+        <Textarea
+          name="category_description"
+          label="Description"
+          value={category.category_description}
+          onChange={onChange}
+          error={errors.title}
+        />
+
+        <input
+          type="submit"
+          disabled={loading}
+          value={loading ? 'Saving...' : 'Save'}
+          className="btn btn-primary"
+          onClick={onSave}
+        />
+      </form>
+    </div>
+  );
 };
 
 CategoryForm.propTypes = {
-    category: PropTypes.object.isRequired,
-    onSave: PropTypes.func.isRequired,
-    onChange: PropTypes.func.isRequired,
-    loading: PropTypes.bool,
-    errors: PropTypes.object
+  category: PropTypes.object.isRequired,
+  onSave: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  loading: PropTypes.bool,
+  errors: PropTypes.object,
 };
 
 export default CategoryForm;
